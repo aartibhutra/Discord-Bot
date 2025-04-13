54ceb93eb138a4ceb5f087527e93948c083499e5
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-13 16:12:11 +0530

    add changes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 // create a virtual client , with the help of the client we interact with the discord api
 // intents are used to specify which events we want to receive from the discord api(like giving a permission to the bot to read messages in a channel)
-const client = new Client({ intents: [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds , 
+    GatewayIntentBits.GuildMessages , GatewayIntentBits.MessageContent] });
+
+// setup listeners for the client
+client.on('messageCreate' , (message) => {
+    // console.log(message.content); // log the message content to the console
+    if(message.author.bot) return; // ignore messages from bots
+    message.reply({
+        content : "Hi From the bot!",
+    });
+});
+
+// login to the discord api using the bot token
+client.login(process.env.DISCORD_TOKEN);
